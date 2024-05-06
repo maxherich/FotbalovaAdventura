12daf3dcfa02ae2e7f689646db5b1b5157e20b84
Max <max.herich@gmail.com>
2024-05-06 10:59:32 +0200

    Create variables for Match class.

--- src/Match.java
+++ src/Match.java
@@ -1,2 +1,11 @@
 public class Match {
+    private Team homeTeam;
+    private Team awayTeam;
+    private int homeTeamsGoals;
+    private int awaysTeamsGoals;
+
+    public Match(Team homeTeam, Team awayTeam) {
+        this.homeTeam = homeTeam;
+        this.awayTeam = awayTeam;
+    }
 }

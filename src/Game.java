305592ced1254518f329fbf7fc14503e27432a73
Max <max.herich@gmail.com>
2024-05-11 19:16:48 +0200

    Add constructor in Game class.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Game {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public Game (){
+        System.out.println("Enter name of your player");
+        Player player = new Player();
+        player.setName(scanner.next());
+
+
+
+    }
 }

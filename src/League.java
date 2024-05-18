f032a80bc05bf1017c7ebf81822bd222ccfd782d
Max <max.herich@gmail.com>
2024-05-18 16:40:29 +0200

    Create League Class.

---
+++ src/League.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class League {
+    private TeamLeague league;
+    private ArrayList <Team> teams = new ArrayList<>();
+    private
+
+    public void addteam (){
+
+    }
+}

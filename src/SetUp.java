34efb644a77c3513bfacc9e8d9b7dee4b352371b
Max <max.herich@gmail.com>
2024-05-18 17:04:30 +0200

    Create SetUp class.

---
+++ src/SetUp.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class SetUp {
+    HashMap<String, Command> commands = new HashMap<>();
+    ArrayList<Team> rating1teams = new ArrayList<>();
+    ArrayList<Team> rating2teams = new ArrayList<>();
+    ArrayList<Team> rating3teams = new ArrayList<>();
+    ArrayList<Team> rating4teams = new ArrayList<>();
+    ArrayList<Team> rating5teams = new ArrayList<>();
+    ArrayList<Team> rating6teams = new ArrayList<>();
+    ArrayList<Team> rating7teams = new ArrayList<>();
+}

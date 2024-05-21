140bcd99a7c53c4ed09f1728bdd692bde564a258
maxherich0 <herich@pixelfield.cz>
2024-05-21 17:45:41 +0200

    Add new Game.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-
+        try {
+            Game game = new Game();
+        }catch (Exception e){
+            System.out.println(e);
+        }
     }
 }

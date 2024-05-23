88ba87d17fdc87b865cf754ecce52c10ddf1118c
maxherich0 <herich@pixelfield.cz>
2024-05-23 13:51:30 +0200

    Complete MediumTraining Command.

--- src/MediumTraining.java
+++ src/MediumTraining.java
@@ -1,16 +1,12 @@
 public class MediumTraining extends Command{
     @Override
-    public void execute(Player player, int training) {
+    public int execute(Player player, int training) {
         if (training<5){
             player.setRating(player.getRating()+1);
             System.out.println("You gain: +1 rating");
         }else {
             System.out.println("It is not training day");
         }
-    }
-
-    @Override
-    public boolean exit() {
-        return false;
+        return training;
     }
 }

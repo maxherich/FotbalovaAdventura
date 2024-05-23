060d31503ad65aa49c1f47b56f5fe70be10b2d55
maxherich0 <herich@pixelfield.cz>
2024-05-23 13:51:43 +0200

    Complete HardTraining Command.

--- src/HardTraining.java
+++ src/HardTraining.java
@@ -1,17 +1,18 @@
 public class HardTraining extends Command{
     @Override
-    public void execute(Player player, int training) {
-        if (training<5){
-            player.setEnergy(player.getEnergy()-30);
-            player.setRating(player.getRating()+3);
+    public int execute(Player player, int training) {
+        if (training < 5 && player.getEnergy() > 30) {
+            player.setEnergy(player.getEnergy() - 30);
+            player.setRating(player.getRating() + 3);
             System.out.println("You gain: -30 energy and +3 rating");
-        }else {
+        }else if (training>4){
             System.out.println("It is not training day");
+        }else{
+            training--;
+            System.out.println("You dont have enough energy for this training");
         }
+        return training;
     }
 
-    @Override
-    public boolean exit() {
-        return false;
-    }
 }
+

70971a8f9b11ad786d2252f5081ac1a753d495d2
maxherich0 <herich@pixelfield.cz>
2024-05-23 13:57:41 +0200

    Create constructor for Product Class.

--- src/Product.java
+++ src/Product.java
@@ -1,4 +1,9 @@
 public class Product {
     private String name;
     private int prize;
+
+    public Product(String name, int prize) {
+        this.name = name;
+        this.prize = prize;
+    }
 }

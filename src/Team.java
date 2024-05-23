76fce49500e7c528ab07bb14b279c0b927fe7e1f
maxherich0 <herich@pixelfield.cz>
2024-05-23 13:58:35 +0200

    Create constructor for Team Class.

--- src/Team.java
+++ src/Team.java
@@ -1,8 +1,54 @@
 public class Team {
-    private double teamRating;
-    private TeamStyle teamStyle;
-    private double trustFor30min;
-    private double trustFor60min;
-    private double trustFor90min;
-    private TeamLeague league;
+    private String name;
+    private int teamRating;
+    private String teamStyle;
+    private League league;
+    private int salary;
+    private int serialNumber;
+    private double leaguePoints;
+
+    public Team(String name, int teamRating, String teamStyle, League league, int serialNumber) {
+        this.name = name;
+        this.teamRating = teamRating;
+        this.teamStyle = teamStyle;
+        this.league = league;
+        this.serialNumber = serialNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getTeamRating() {
+        return teamRating;
+    }
+
+    public String getTeamStyle() {
+        return teamStyle;
+    }
+
+    public League getLeague() {
+        return league;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public int getSerialNumber() {
+        return serialNumber;
+    }
+
+    public double getLeaguePoints() {
+        return leaguePoints;
+    }
+
+    @Override
+    public String toString() {
+        return name + "";
+    }
 }

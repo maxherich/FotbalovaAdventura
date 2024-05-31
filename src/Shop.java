10a6afd756521c3db2b116d428fc1667009d6622
maxherich0 <herich@pixelfield.cz>
2024-05-31 13:29:16 +0200

    Create Shop command.

---
+++ src/Shop.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Shop extends OtherCommand{
+    @Override
+    public void execute(Player player, ArrayList shop) {
+        System.out.println(shop);
+
+    }
+}

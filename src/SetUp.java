b7e56087df2d803b18a39c5febdfb7e686abcffb
maxherich0 <herich@pixelfield.cz>
2024-05-31 13:32:28 +0200

    Add commands to HashMap.

--- src/SetUp.java
+++ src/SetUp.java
@@ -9,7 +9,10 @@ import java.util.Scanner;
 public class SetUp {
     private Random random = new Random();
     private Scanner scanner = new Scanner(System.in);
-    private HashMap<String, Command> trainingCommands = new HashMap<>();
+    private HashMap<String, TrainingCommand> trainingCommands = new HashMap<>();
+    private HashMap<String, MatchCommand> matchCommands = new HashMap<>();
+    private HashMap<String, OtherCommand> otherCommands = new HashMap<>();
+    private ArrayList<Team> teams = new ArrayList<>();
     private ArrayList<Team> rating1teams = new ArrayList<>();
     private ArrayList<Team> rating2teams = new ArrayList<>();
     private ArrayList<Team> rating3teams = new ArrayList<>();
@@ -20,6 +23,7 @@ public class SetUp {
     private League FortunaLiga = new League("FortunaLiga", 3);
     private League Bundesliga = new League("Bundesliga", 5);
     private League PremierLeague = new League("PremierLeague", 8);
+    private ArrayList <Team> UCLteams = new ArrayList<>();
 
     private ArrayList <Product> shop = new ArrayList<>();
     private Product energyGel = new Product("energyGel", 50);
@@ -30,6 +34,17 @@ public class SetUp {
         trainingCommands.put("easy",new EasyTraining());
         trainingCommands.put("medium",new MediumTraining());
         trainingCommands.put("hard", new HardTraining());
+
+        matchCommands.put("lefttop",new LeftTop());
+        matchCommands.put("leftbottom", new LeftBottom());
+        matchCommands.put("midtop",new MidTop());
+        matchCommands.put("midbottom",new MidBottom());
+        matchCommands.put("righttop", new RightTop());
+        matchCommands.put("rightbottom", new RightBottom());
+
+        otherCommands.put("shop",new Shop());
+        otherCommands.put("boots", new BuyBoots());
+        otherCommands.put("energygel", new BuyEnergyGel());
     }
 
     private String text;
@@ -69,12 +84,13 @@ public class SetUp {
                 rating7teams.add(newTeam);
             }
             if (strings[1].equals(PremierLeague.getName())){
-                PremierLeague.addteam(newTeam);
+                PremierLeague.addTeam(newTeam);
             }else if (strings[1].equals(Bundesliga.getName())){
-                Bundesliga.addteam(newTeam);
+                Bundesliga.addTeam(newTeam);
             }else if (strings[1].equals(FortunaLiga.getName())){
-                FortunaLiga.addteam(newTeam);
+                FortunaLiga.addTeam(newTeam);
             }
+            teams.add(newTeam);
         }
     }
 
@@ -82,7 +98,7 @@ public class SetUp {
     public void newContract (Player player){
         teamsWithNewContract.clear();
         if (player.getRating() < 200){
-            System.out.println("Rating of these teams is "+ 1 + " and weekly salary is " + 10 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 1 + " and weekly salary is " + 10 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating1teams.get(random.nextInt(rating1teams.size()));
                 teamsWithNewContract.add(team);
@@ -91,7 +107,7 @@ public class SetUp {
             rating1teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else if (player.getRating() < 300){
-            System.out.println("Rating of these teams is "+ 2 + " and weekly salary is " + 12 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 2 + " and weekly salary is " + 12 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating2teams.get(random.nextInt(rating2teams.size()));
                 teamsWithNewContract.add(team);
@@ -100,7 +116,7 @@ public class SetUp {
             rating2teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else if (player.getRating() < 400){
-            System.out.println("Rating of these teams is "+ 3 + " and weekly salary is " + 15 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 3 + " and weekly salary is " + 15 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating3teams.get(random.nextInt(rating3teams.size()));
                 teamsWithNewContract.add(team);
@@ -109,7 +125,7 @@ public class SetUp {
             rating3teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else if (player.getRating() < 500){
-            System.out.println("Rating of these teams is "+ 4 + " and weekly salary is " + 20 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 4 + " and weekly salary is " + 20 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating4teams.get(random.nextInt(rating4teams.size()));
                 teamsWithNewContract.add(team);
@@ -118,7 +134,7 @@ public class SetUp {
             rating4teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else if (player.getRating() < 600){
-            System.out.println("Rating of these teams is "+ 5 + " and weekly salary is " + 25 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 5 + " and weekly salary is " + 25 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating5teams.get(random.nextInt(rating5teams.size()));
                 teamsWithNewContract.add(team);
@@ -127,7 +143,7 @@ public class SetUp {
             rating5teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else if (player.getRating() < 700){
-            System.out.println("Rating of these teams is "+ 6 + " and weekly salary is " + 32 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 6 + " and weekly salary is " + 32 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating6teams.get(random.nextInt(rating6teams.size()));
                 teamsWithNewContract.add(team);
@@ -136,7 +152,7 @@ public class SetUp {
             rating6teams.addAll(teamsWithNewContract);
             System.out.println(teamsWithNewContract);
         }else {
-            System.out.println("Rating of these teams is "+ 7 + " and weekly salary is " + 40 + ".\nWrite name of that team you want to play in next season.");
+            System.out.println("Rating of these teams is "+ 7 + " and weekly salary is " + 40 + "$\nWrite name of that team you want to play in next season.");
             for (int i = 0; i < 3; i++){
                 Team team = rating7teams.get(random.nextInt(rating7teams.size()));
                 teamsWithNewContract.add(team);
@@ -166,7 +182,7 @@ public class SetUp {
                 System.out.println("Your answer does not equal to any option");
             }
         }
-        return "You have signed new contract!";
+        return "You have signed new contract!\n";
     }
 
     public void setShop() {
@@ -174,8 +190,24 @@ public class SetUp {
         shop.add(boots);
     }
 
-    public HashMap<String, Command> getTrainingCommands() {
+    public ArrayList<Product> getShop() {
+        return shop;
+    }
+
+    public HashMap<String, TrainingCommand> getTrainingCommands() {
         return trainingCommands;
+    }
+
+    public HashMap<String, MatchCommand> getMatchCommands() {
+        return matchCommands;
+    }
+
+    public HashMap<String, OtherCommand> getOtherCommands() {
+        return otherCommands;
+    }
+
+    public ArrayList<Team> getTeams() {
+        return teams;
     }
 
     public ArrayList<Team> getRating1teams() {
@@ -218,9 +250,7 @@ public class SetUp {
         return PremierLeague;
     }
 
-    @Override
-    public String toString() {
-        return teamsWithNewContract + "";
+    public ArrayList<Team> getUCLteams() {
+        return UCLteams;
     }
-
 }

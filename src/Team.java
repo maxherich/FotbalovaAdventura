3b0f050034af5ef2cdc6acc6fc197bbe27fa863c
maxherich0 <herich@pixelfield.cz>
2024-05-31 13:34:33 +0200

    Create methods for Team Class.

--- src/Team.java
+++ src/Team.java
@@ -1,11 +1,13 @@
-public class Team {
+import java.util.Random;
+
+public class Team implements Comparable<Team> {
     private String name;
     private int teamRating;
     private String teamStyle;
     private League league;
     private int salary;
     private int serialNumber;
-    private double leaguePoints;
+    private int leaguePoints;
 
     public Team(String name, int teamRating, String teamStyle, League league, int serialNumber) {
         this.name = name;
@@ -13,6 +15,53 @@ public class Team {
         this.teamStyle = teamStyle;
         this.league = league;
         this.serialNumber = serialNumber;
+    }
+
+    public void setPoints (){
+        Random random = new Random();
+        if (teamRating == 1){
+            this.leaguePoints = random.nextInt(20)+11;
+        }else if (teamRating == 2){
+            if (league.getName().equals("FortunaLiga")){
+                this.leaguePoints = random.nextInt(30)+21;
+            }else if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(20)+11;
+            }
+        }else if (teamRating == 3){
+            if (league.getName().equals("FortunaLiga")){
+                this.leaguePoints = random.nextInt(30)+31;
+            }else if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(20)+21;
+            }else if (league.getName().equals("PremierLeague")){
+                this.leaguePoints = random.nextInt(30)+11;
+            }
+        }else if (teamRating == 4){
+            if (league.getName().equals("FortunaLiga")){
+                this.leaguePoints = random.nextInt(30)+51;
+            }else if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(20)+31;
+            }else if (league.getName().equals("PremierLeague")){
+                this.leaguePoints = random.nextInt(20)+31;
+            }
+        }else if (teamRating == 5){
+            if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(30)+41;
+            }else if (league.getName().equals("PremierLeague")){
+                this.leaguePoints = random.nextInt(20)+41;
+            }
+        }else if (teamRating == 6){
+            if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(20)+61;
+            }else if (league.getName().equals("PremierLeague")){
+                this.leaguePoints = random.nextInt(30)+51;
+            }
+        }else if (teamRating == 7){
+            if (league.getName().equals("Bundesliga")){
+                this.leaguePoints = random.nextInt(20)+71;
+            }else if (league.getName().equals("PremierLeague")){
+                this.leaguePoints = random.nextInt(30)+71;
+            }
+        }
     }
 
     public String getName() {
@@ -43,12 +92,21 @@ public class Team {
         return serialNumber;
     }
 
-    public double getLeaguePoints() {
+    public void setLeaguePoints(int leaguePoints) {
+        this.leaguePoints = leaguePoints;
+    }
+
+    public int getLeaguePoints() {
         return leaguePoints;
     }
 
     @Override
     public String toString() {
-        return name + "";
+        return name + " - (league: " + league + " - teamStyle: " + teamStyle + ")";
+    }
+
+    @Override
+    public int compareTo(Team o) {
+        return Integer.compare(o.leaguePoints, this.leaguePoints);
     }
 }

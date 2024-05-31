ff34b9c85dcd41b5cbb60f7ee6f5be2e754751ad
maxherich0 <herich@pixelfield.cz>
2024-05-31 13:25:25 +0200

    Finish EasyTraining command.

--- src/EasyTraining.java
+++ src/EasyTraining.java
@@ -1,9 +1,9 @@
-public class EasyTraining extends Command{
+public class EasyTraining extends TrainingCommand {
     @Override
     public int execute(Player player, int training) {
         if (training<5){
             player.setEnergy(player.getEnergy()+20);
-            System.out.println("You gain: +20 energy");
+            System.out.println("You gain: +20 energy\n");
         }else {
             System.out.println("It is not training day");
         }
